b5ed3b307846c88406f0f11610ba6ae86ea780dc
ryo <65832968+ryosafadi@users.noreply.github.com>
2024-11-25 13:09:44 -0800

    add image spawning

--- Assets/Scripts/SpawnImages.cs
+++ Assets/Scripts/SpawnImages.cs
@@ -3,18 +3,24 @@ using UnityEngine;
 public class SpawnImages : MonoBehaviour
 {
     [SerializeField] private GameObject image;
+    [SerializeField] private Material[] materials;
 
     void Start()
     {
-        Instantiate(image, new Vector3(6.8f, 0.1f, 6.8f), Quaternion.identity);
-        /*
-        Instantiate(image, new Vector3(6.8f, 0.1f, 4f), Quaternion.identity);
-        Instantiate(image, new Vector3(6.8f, 0.1f, 1.2f), Quaternion.identity);
-        Instantiate(image, new Vector3(4f, 0.1f, 6.8f), Quaternion.identity);
-        Instantiate(image, new Vector3(4f, 0.1f, 4f), Quaternion.identity);
-        Instantiate(image, new Vector3(4f, 0.1f, 1.2f), Quaternion.identity);
-        Instantiate(image, new Vector3(1.2f, 0.1f, 6.8f), Quaternion.identity);
-        Instantiate(image, new Vector3(1.2f, 0.1f, 4f), Quaternion.identity);
-        Instantiate(image, new Vector3(1.2f, 0.1f, 1.2f), Quaternion.identity); */
+        for (float i = 1.2f; i <= 6.8f; i += 2.8f)
+        {
+            for (float j = 1.2f; j <= 6.8f; j += 2.8f)
+            {
+                Vector3 localPosition = new Vector3(i, 0.1f, j);
+                Vector3 worldPosition = transform.TransformPoint(localPosition);
+                GameObject newImage = Instantiate(image, worldPosition, transform.rotation, transform);
+
+                MeshRenderer meshRenderer = newImage.GetComponent<MeshRenderer>();
+                if (meshRenderer != null)
+                {
+                    meshRenderer.material = materials[Random.Range(0, materials.Length)];
+                }
+            }
+        }
     }
 }
