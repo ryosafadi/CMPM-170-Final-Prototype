c36a49439fc1104c1b31a4a0b2cafcc41d0b9426
ryo <65832968+ryosafadi@users.noreply.github.com>
2024-12-02 15:32:57 -0800

    add end scene

---
+++ Assets/Scripts/FinalScore.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+
+public class FinalScore : MonoBehaviour
+{
+    void Start()
+    {
+        TextMeshProUGUI textUI = GetComponent<TextMeshProUGUI>();
+
+        textUI.text = $"Final Score: {StaticData.GetPoints()}";
+    }
+}
